50128ecc95ed5f6bbf5d8bdbb783e50d4e954fbc
Saif Ali <saif.ali@mountblue.io>
2019-10-19 11:49:15 +0530

    Updates

---
+++ precache-manifest.869d03d803de4c9d32c5c7abc0577d0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba866c6e288c849b201440e160b9e16",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "773e29b8d7fac4f3a715",
+    "url": "/react-app/static/css/main.6ce48526.chunk.css"
+  },
+  {
+    "revision": "4c7f7a893e830198b47c",
+    "url": "/react-app/static/js/2.2cd1d228.chunk.js"
+  },
+  {
+    "revision": "773e29b8d7fac4f3a715",
+    "url": "/react-app/static/js/main.82edac12.chunk.js"
+  },
+  {
+    "revision": "4b738ffa00d75678252e",
+    "url": "/react-app/static/js/runtime-main.94c8b542.js"
+  }
+]);
