614c0ae77bca018530660b901596a863a6197f22
Saif Ali <saif.ali@mountblue.io>
2019-10-19 10:44:51 +0530

    Added Heading and refactored search box

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
 			monsters: [],
 			searchField: ''
 		};
+
+		// this.handleChange = this.handleChange.bind(this);
 	}
 
 	componentDidMount() {
@@ -21,18 +23,22 @@ class App extends Component {
 			.then((users) => this.setState({ monsters: users }));
 	}
 
+	handleChange = (e) => {
+		this.setState({ searchField: e.target.value });
+	}
+
 	render() {
     const { monsters, searchField } = this.state;
     const filterMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     
     return (
 			<div className="App">
+				<h1> Monsters Rolodex </h1>
           <SearchBox 
             placeholder='search monsters'
-            handleChange={e => this.setState({ searchField: e.target.value })}
+            handleChange={this.handleChange}
             />
-
-				  <CardList monsters={filterMonsters} />
+  			  <CardList monsters={filterMonsters} />
 			</div>
 		);
 	}
